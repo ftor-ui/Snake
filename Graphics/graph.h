ec3bdedec219751efb675bfa2f2fc473dfef5c9b
ftor-ui <52377049+ftor-ui@users.noreply.github.com>
2021-03-23 13:57:39 +0300

    Update graph.h

--- Graphics/graph.h
+++ Graphics/graph.h
@@ -10,13 +10,21 @@ public:
 	~Window();
 	bool isOpen();
 	void render();
+	bool setFont(char const *);
 
 	sf::RenderWindow renderWindow;
+	sf::Font font;
 private:
 	Window(Window const &);
 	Window & operator=(Window const &);
 };
 
 void printSquare(Window &, int, int, int const, char const *);
+
+void printBorder(Window &, int, int, int const);
+
+void printString(Window &, int, int, int, char const *);
+
+sf::ConvexShape printButton(Window &, int, int, int, int, char const *);
 
 #endif
